208db3451d6008e1dc2e629cd1913e94a336ab8b
Melihcan Fidan <melihcan.fi@gmail.com>
2024-03-29 15:17:53 +0300

    update

--- haberler/views.py
+++ haberler/views.py
@@ -1,36 +1,21 @@
 from django.shortcuts import render
 from .models import*
-from newsapi import NewsApiClient
 
-# Init
-newsapi = NewsApiClient(api_key='bbd773aaa75649df8e65214bcc9b894d')
+import requests
 
-# /v2/top-headlines
-top_headlines = newsapi.get_top_headlines(q='bitcoin',
-                                          sources='bbc-news,the-verge',
-                                          category='business',
-                                          language='en',
-                                          country='us')
+url = ('https://newsapi.org/v2/everything?'
+       'q=Apple&'
+       'from=2024-03-29&'
+       'sortBy=popularity&'
+       'apiKey=bbd773aaa75649df8e65214bcc9b894d')
 
-# /v2/everything
-all_articles = newsapi.get_everything(q='bitcoin',
-                                      sources='bbc-news,the-verge',
-                                      domains='bbc.co.uk,techcrunch.com',
-                                      from_param='2017-12-01',
-                                      to='2017-12-12',
-                                      language='en',
-                                      sort_by='relevancy',
-                                      page=2)
-
-# /v2/top-headlines/sources
-sources = newsapi.get_sources()
+response = requests.get(url)
 
 # Create your views here.
 
 def index(request):
-
     context = {
-        
+        'news': response,
     }
-    
-    return render(request,'haberler/index.html')
+    print(context)
+    return render(request,'haberler/index.html', context=context)
